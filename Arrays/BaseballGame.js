c078e909972a64acb81bbc7cf9ba1c6610d4ac3c
Ankita Sood <ankitasood2000@gmail.com>
2025-07-18 16:18:36 +0530

    Corrected answer for 682. Baseball Game

--- Arrays/BaseballGame.js
+++ Arrays/BaseballGame.js
@@ -1,15 +1,20 @@
 var calPoints = function(operations) {
     let output=[];
     for(let i=0;i<operations.length;i++) {
-        if(operations.charCodeAt(i) <=48 && operations.charCodeAt(i) >= 57) {
-            output.push(operations[i])
+        if(Number(operations[i])) {
+            output.push(Number(operations[i]))
         } else if(operations[i] == '+') {
-            output.push(operations[i-2] + operations[i-1]);
+            output.push(Number(output[output.length-2]) + Number(output[output.length-1]));
         } else if(operations[i] == 'D') {
-            output.push(operations[i-2] * operations[i-1])
+            output.push(2 * Number(output[output.length-1]));
         } else if(operations[i] == 'C'){
             output.pop();
         }
     }
-    return output;
+
+    let sum =0;
+    for(let i=0;i<output.length;i++) {
+        sum += output[i]
+    }
+    return sum;
 };

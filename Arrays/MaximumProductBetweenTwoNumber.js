e88133f5bd7ebc93346c3cdb58a8a3b04dc7857c
Ankita Sood <ankitasood2000@gmail.com>
2025-07-28 15:47:17 +0530

    1913. Maximum Product Difference Between Two Pairs

---
+++ Arrays/MaximumProductBetweenTwoNumber.js
@@ -0,0 +1,6 @@
+var maxProductDifference = function(nums) {
+    nums.sort((a,b) => a-b);
+    return  (nums[nums.length-1] * nums[nums.length-2]) - (nums[0] * nums[1]);
+};
+
+console.log(maxProductDifference([5,6,2,7,4]))

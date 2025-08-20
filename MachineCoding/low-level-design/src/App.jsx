ca8d8ae296bc2cd1cf327c997ef3f0e0be82c91a
Ankita Sood <ankitasood2000@gmail.com>
2025-08-20 17:22:31 +0530

    React simple counter

--- MachineCoding/low-level-design/src/App.jsx
+++ MachineCoding/low-level-design/src/App.jsx
@@ -1,12 +1,9 @@
-import ChipsInput from "./chips_input/ChipsInput";
-import CopyClipboard from "./copy_to_clipboard/CopyClipboard";
-import LeapYear from "./leap_year/LeapYear";
-import Password from "./password_checker/Password";
+import Counter from "./Counter/Counter";
 
 function App() {
   return (
     <main>
-      <Password />
+      <Counter />
     </main>
   );
 }

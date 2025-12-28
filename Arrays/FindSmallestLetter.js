f09f866166ad4f7dd2ec041db1edfaed7a38eafa
Ankita Sood <ankitasood2000@gmail.com>
2025-12-28 10:37:51 +0530

    744. Find Smallest Letter Greater Than Target

---
+++ Arrays/FindSmallestLetter.js
@@ -0,0 +1,10 @@
+// 744. Find Smallest Letter Greater Than Target
+var nextGreatestLetter = function (letters, target) {
+  for (let i = 0; i < letters.length; i++) {
+    if (letters[i] > target) {
+      return letters[i];
+    }
+  }
+  return letters[0];
+};
+console.log(nextGreatestLetter(["x", "x", "y", "y"], "z"));
